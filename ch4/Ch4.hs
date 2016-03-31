530c63f24635d8428afda8a992bc162ce7536be9
Trevor D. Miller <trevordmiller@icloud.com>
2016-03-31 14:09:27 -0600

    Finishing chapter 4 exercises

--- ch4/Ch4.hs
+++ ch4/Ch4.hs
@@ -62,8 +62,92 @@ changeMood _ = Woot
 -- Kind of like "desctructuring"
 twoOfThree (_, x, _) = x
 
+
+awesome = ["Papuchon", "curry", ":)"]
+alsoAwesome = ["Quake", "The Simons"]
+allAwesome = [awesome, alsoAwesome]
+
+
+-- ------------------------------------------------------------------------------------
+-- 4.7
+-- ------------------------------------------------------------------------------------
+
+-- 1
+-- Given the definition of length above, what would the type signature be? 
 -- length' :: [a] -> Int
+-- How many arguments, of what type does it take?
+-- 1 argument of type 'List'
+-- What is the type of the result it evaluates to?
+-- Int
+
+-- 2
+-- a) 5
+-- b) 3
+-- c) 2
+-- d) 5
+
+-- 3
+-- 6 / length [1, 2, 3] doesn't work because length outputs a string and the (/) operator expects fractional types
+
+-- 4
+-- Use 'div'
+
+-- 5
+-- What is the type of the expression 2 + 3 == 5? What would we expect as a result?
+-- Bool, True
+
+-- 6
+-- Bool, False
+
+-- 7
+-- "length allAwesome == 2" should result to a Bool of True
+-- "length [1, 'a', 3, 'b']" should result in an error since a list have to have elements of the same type
+-- "length allAwesome + length awesome" should result in an Int of 5
+-- "(8 == 8) && ('b' < 'a')" should result in a Bool of False
+-- "(8 == 8) && 9" should result in an error because 9 doesn't evaluate to a Bool, which the '&&' operator requires by its type signature
+
+-- 8
+isPalendrome :: (Eq a) => [a] -> Bool
+isPalendrome word = word == (reverse word)
+
+-- 9
+absoluteNumber :: Integer -> Integer
+absoluteNumber number =
+  if (number < 0)
+     then number * (-1)
+     else number
+
+-- 10
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f fstTuple sndTuple = ((snd fstTuple, snd sndTuple), (fst fstTuple, fst sndTuple))
+
+-- 1
+-- c
+
+-- 2
+-- b
+
+-- 3
+-- a
+
+-- 4
+-- d
 
 main :: IO ()
 main = do
-  putStrLn $ show $ twoOfThree (1, 2, 3)
+  putStrLn $ show $ length [1, 2, 3, 4, 5]
+  putStrLn $ show $ length [(1, 2), (2, 3), (3, 4)]
+  putStrLn $ show $ length allAwesome
+  putStrLn $ show $ length (concat allAwesome)
+  putStrLn $ show $ div 6 (length [1, 2, 3])
+  putStrLn $ show $ 2 + 3 == 5
+  putStrLn $ show $ let x = 5 in x + 3 == 5
+  putStrLn $ show $ length allAwesome == 2
+  putStrLn $ show $ length allAwesome + length awesome
+  putStrLn $ show $ (8 == 8) && ('b' < 'a')
+  putStrLn $ show $ isPalendrome "Trevor"
+  putStrLn $ show $ isPalendrome "abcba"
+  putStrLn $ show $ absoluteNumber 42
+  putStrLn $ show $ absoluteNumber (-42)
+  putStrLn $ show $ f ('c', 'a') ('d', 'b')
+  putStrLn $ show $ (\x -> x) 5
